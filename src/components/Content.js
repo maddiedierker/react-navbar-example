07bdb8199f379d140a8c8b128b5560689eee22fc
Madelyn Kasula <madelynkasula@gmail.com>
2017-12-02 11:32:02 -0600

    Add placeholder content and styling

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import { placeholderContent } from '../placeholderContent'
 
 export default class Content extends Component {
   render() {
-    return <div className="content-container">
-      I'm the content!
-    </div>
+    return <div
+      className="content-container"
+      dangerouslySetInnerHTML={{__html: placeholderContent}}
+    />
   }
 }
