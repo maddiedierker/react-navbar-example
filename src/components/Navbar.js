0d56bbaf08384a34dc405c5c791f74fa250a2366
Madelyn Kasula <madelynkasula@gmail.com>
2017-12-02 11:26:52 -0600

    Add navbar links and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 
 export default class Navbar extends Component {
+  renderNavbar = () => {
+    return <div className="navbar">
+      <div className="navbar-link">Home</div>
+      <div className="navbar-link">About</div>
+      <div className="navbar-link">Contact</div>
+    </div>
+  }
+
   render() {
     return <div className="navbar-container">
-      I'm the navbar!
+      {this.renderNavbar()}
     </div>
   }
 }
