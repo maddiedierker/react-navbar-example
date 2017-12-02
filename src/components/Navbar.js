ff1f65ed66cac0fb64215ab8a96fc57948a65ff9
Madelyn Kasula <madelynkasula@gmail.com>
2017-12-02 11:34:03 -0600

    Add isOpen state to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 
 export default class Navbar extends Component {
+  state = {
+    isOpen: true
+  }
+
   renderNavbar = () => {
+    if (!this.state.isOpen) {
+      return null
+    }
+
     return <div className="navbar">
       <div className="navbar-link">Home</div>
       <div className="navbar-link">About</div>
