fcd732cc2e62651a08f2213b009d0d82addd1da3
Madelyn Kasula <madelynkasula@gmail.com>
2017-12-02 12:18:37 -0600

    Move App component export for consistency

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Navbar from './Navbar'
 import Content from './Content'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="app">
@@ -12,5 +12,3 @@ class App extends Component {
     )
   }
 }
-
-export default App
